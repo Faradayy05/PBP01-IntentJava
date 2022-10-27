852ae7dccc69a1097b8086ca27dec83989bb15fd
Faradayy05 <muhammadfarisadityapratama05@gmail.com>
2022-10-27 19:39:45 +0700

    Update Date

--- app/src/main/java/com/pbp/formdesign/MainActivity.java
+++ app/src/main/java/com/pbp/formdesign/MainActivity.java
@@ -2,11 +2,15 @@ package com.pbp.formdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
 
         //Button
         Button etButton = findViewById(R.id.etButton);
+
+        //Calendar
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        etTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month = month + 1;
+                        String date = day+"/"+month+"/"+year;
+                        etTanggal.setText(date);
+                    }
+                },year,month,day);
+                dialog.show();
+            }
+        });
 
         etButton.setOnClickListener(new View.OnClickListener() {
             @Override
